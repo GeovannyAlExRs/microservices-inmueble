328c99599f1c4741ec85d1dcc66fc750967f7a89
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-05-15 16:39:26 -0500

    Initial commit model, repository and app.properties

---
+++ src/main/java/com/company/dtk/microservicesinmueble/MicroservicesInmuebleApplication.java
@@ -0,0 +1,13 @@
+package com.company.dtk.microservicesinmueble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesInmuebleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesInmuebleApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/company/dtk/microservicesinmueble/model/Inmueble.java
@@ -0,0 +1,31 @@
+package com.company.dtk.microservicesinmueble.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name="inmueble")
+public class Inmueble {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", length = 150, nullable = false)
+    private String name;
+
+    @Column(name = "address", length = 500, nullable = false)
+    private String address;
+
+    @Column(name = "photo", length = 1024, nullable = true)
+    private String photo;
+
+    @Column(name = "price", nullable = false)
+    private Double price;
+
+    @Column(name = "date", nullable = false)
+    private LocalDateTime date;
+}
